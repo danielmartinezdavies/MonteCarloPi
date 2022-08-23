266d40c171b6ef0a0d2499b0d2025ed87af728a3
danielmartinezdavies <danielmartinezdavies@gmail.com>
2022-08-23 10:38:08 +0200

    Refactor drawing of frames.

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,36 @@ void checkEvents(sf::RenderWindow &window) {
             window.close();
             exit(0);
         }
-
     }
 }
+
+void drawTopText(sf::RenderWindow &window, sf::Text &text, sf::Text &text_number_points, std::string &pi_estimate, int num_points_total) {
+    text.setString(          "Pi estimate:            " + pi_estimate);
+    window.draw(text);
+    std::string num_points = "Number of points:  " + std::to_string(num_points_total);
+    text_number_points.setString(num_points);
+    window.draw(text_number_points);
+}
+
+sf::Font loadFont() {
+    sf::Font font;
+    if (!font.loadFromFile("../resources/arial.ttf"))
+    {
+        exit(-1);
+    }
+    return font;
+}
+sf::Text createText(sf::Font &font, int character_size, const sf::Color &color, float x, float y) {
+    sf::Text text;
+    text.setFont(font);
+    text.setCharacterSize(character_size); // in pixels, not points!
+    text.setFillColor(color);
+    text.setPosition(x,y);
+    return text;
+}
+
 int main() {
+
     boost::random::sobol s_rng(2);
     typedef boost::variate_generator<boost::random::sobol&, boost::uniform_01<float> > quasi_random_gen_t;
 
@@ -29,85 +55,62 @@ int main() {
 
     std::random_device non_det;
     std::mt19937 generator (123);
-    std::uniform_real_distribution<float> dis(0.0, 1.0 * 800);
+    std::uniform_real_distribution<float> dis(0.0, 1.0);
     //double randomRealBetweenZeroAndOne = ;
 
+    sf::Font font = loadFont();
 
-    sf::Font font;
-    if (!font.loadFromFile("../resources/arial.ttf"))
-    {
-        exit(-1);
-    }
-
-    sf::Text text;
-    text.setFont(font);
-
-    text.setCharacterSize(24); // in pixels, not points!
-    text.setFillColor(sf::Color::White);
-
-    sf::Text text_number_points;
-    text_number_points.setFont(font);
-
-    text_number_points.setCharacterSize(24); // in pixels, not points!
-    text_number_points.setFillColor(sf::Color::White);
-    text_number_points.setPosition(0,24);
+    sf::Text pi_estimate_text = createText(font, 24, sf::Color::White, 1,  0);
+    sf::Text number_points_text = createText(font, 24, sf::Color::White, 1,  24);
 
     sf::RenderWindow window(sf::VideoMode(800, 800), "MonteCarlo Pi");
     window.clear(sf::Color::Black);
 
-    float radius = (float)window.getSize().x / 2 * 3/4;
-    float side_size = radius * 2;
+    float circle_radius = (float)window.getSize().x / 2 * 3/4;
+    float side_size = circle_radius * 2;
 
     sf::RectangleShape square(sf::Vector2f(side_size, side_size));
-    float square_start = (float)window.getSize().x / 2 - radius;
+    float square_start = (float)window.getSize().x / 2 - circle_radius;
     square.setPosition( square_start, square_start);
     square.setFillColor(sf::Color::Transparent);
     square.setOutlineThickness(1);
     square.setOutlineColor(sf::Color::White);
 
 
-    sf::CircleShape circle(radius);
-    circle.setOutlineColor(sf::Color::White);
-    circle.setOutlineThickness(1);
+    sf::CircleShape circle(circle_radius);
     circle.setPosition( square_start, square_start);
-
     circle.setFillColor(sf::Color::Transparent);
+    circle.setOutlineThickness(1);
+    circle.setOutlineColor(sf::Color::White);
 
-
+    int num_points_in_circle = 0;
+    int num_points_in_total = 0;
     std::vector<sf::Vertex> point_vector;
-    // run the program as long as the window is open
-    int count_in_circle = 0;
-    int i = 0;
-    while (i < N)
+    while (num_points_in_total < N)
     {
         checkEvents(window);
 
+        window.draw(circle);
+        window.draw(square);
+
         point_vector.emplace_back(sf::Vector2f(gen.operator()()*side_size+square_start, gen.operator()()*side_size+square_start), sf::Color::White);
+
         for(auto & point : point_vector) {
             window.draw(&point, 1, sf::Points);
         }
+        num_points_in_total++;
 
+        float point_x = point_vector.back().position.x - square_start-circle_radius;
+        float point_y = point_vector.back().position.y - square_start-circle_radius;
+        if(point_x*point_x  + point_y*point_y <= circle_radius*circle_radius) num_points_in_circle++;
 
-        // end the current frame
-        //sf::Time delayTime = sf::milliseconds(3);
-        //sf::sleep(delayTime);
-        i++;
-        float point_x = point_vector.back().position.x - square_start-radius;
-        float point_y = point_vector.back().position.y - square_start-radius;
-        if(point_x*point_x  + point_y*point_y <= radius*radius) count_in_circle++;
+        std::string pi_estimate =  std::to_string(4 * float(num_points_in_circle) / float(num_points_in_total));
+        drawTopText(window, pi_estimate_text, number_points_text, pi_estimate, num_points_in_total);
 
-        std::string pi_estimate =  std::to_string(4 * float(count_in_circle) / float(i));
-        text.setString(          "Pi estimate:            " + pi_estimate);
-        window.draw(text);
-        std::string num_points = "Number of points:  " + std::to_string(i);
-        text_number_points.setString(num_points);
-        window.draw(text_number_points);
-
-        window.draw(circle);
-        window.draw(square);
         window.display();
         window.clear();
     }
+
     std::string filename = "rng.jpeg";
     sf::Texture texture;
     texture.create(window.getSize().x, window.getSize().y);
